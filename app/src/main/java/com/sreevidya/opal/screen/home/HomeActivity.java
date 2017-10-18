815a08f8625984ca829ad38832331b663e46b080
Sree Vidya Sastry <sreevidya.sastry12@gmail.com>
2017-10-18 01:59:19 -0500

    Updating Login Layouts

---
+++ app/src/main/java/com/sreevidya/opal/screen/home/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.sreevidya.opal.screen.home;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.sreevidya.opal.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
