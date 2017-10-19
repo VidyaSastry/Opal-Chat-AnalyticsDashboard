2ce7b67318edc66910663356ae9c73c2ec76e4a2
Sree Vidya Sastry <sreevidya.sastry12@gmail.com>
2017-10-18 21:45:30 -0500

    Adding HomeActivity + Tabbed layout

--- app/src/main/java/com/sreevidya/opal/screen/home/HomeActivity.java
+++ app/src/main/java/com/sreevidya/opal/screen/home/HomeActivity.java
@@ -1,15 +1,123 @@
 package com.sreevidya.opal.screen.home;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.sreevidya.opal.R;
+import com.sreevidya.opal.screen.home.fragments.ChatFragment;
+import com.sreevidya.opal.screen.home.fragments.DashboardFragment;
 
 public class HomeActivity extends AppCompatActivity {
+
+    public static final String TAG = "HomeActivity";
+    public static final String CHAT_FRAGMENT_TAG = "Chat";
+    public static final String DASHBOARD_FRAGMENT_TAG = "Dashboard";
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        TabLayout tabLayout =
+                (TabLayout) findViewById(R.id.tab_layout);
+
+        tabLayout.addTab(tabLayout.newTab().setText("CHAT_FRAGMENT_TAG"));
+        tabLayout.addTab(tabLayout.newTab().setText("CHAT_FRAGMENT_TAG"));
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        mViewPager.addOnPageChangeListener(new
+                TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                mViewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+
+        });
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    ChatFragment chatFragment = new ChatFragment();
+                    return chatFragment;
+                case 1:
+                    DashboardFragment dashboardFragment = new DashboardFragment();
+                    return dashboardFragment;
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "CHAT_FRAGMENT_TAG";
+                case 1:
+                    return "CHAT_FRAGMENT_TAG";
+            }
+            return null;
+        }
     }
 }
